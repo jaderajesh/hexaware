548bed6686e8ac908d5abfad22b813029bae15dd
jaderajesh <jaderajesh29@gmail.com>
2022-05-02 09:42:42 +0530

    progs

---
+++ Max.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Max{
+    public static void main(String[] args) {
+      int arr[] = {1,2,5,4,3};
+      Arrays.sort(arr);
+      int n = arr.length;
+      System.out.println(arr[n-2]);
+      }
+  }
